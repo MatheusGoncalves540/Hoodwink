fc06b0325b1932754f3c592ce36c3f7e4c380360
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2025-07-02 21:54:56 -0300

    Atualiza constantes de estado da sala para seguir convenções de nomenclatura em camelCase

--- gameServer/game/room/roomStructs/state.go
+++ gameServer/game/room/roomStructs/state.go
@@ -2,10 +2,10 @@ package roomStructs
 
 // Definindo os estados possíveis da sala
 const (
-	WaitingAction           = "waiting_action"
-	WaitingContest          = "waiting_contest"
-	ResolvingContest        = "resolving_contest"
-	WaitingKamikazeResponse = "waiting_kamikaze_response"
-	FinalizingAction        = "finalizing_action"
-	TurnFinished            = "turn_finished"
+	WaitingAction           = "waitingAction"
+	WaitingContest          = "waitingContest"
+	ResolvingContest        = "resolvingContest"
+	WaitingKamikazeResponse = "waitingKamikazeResponse"
+	FinalizingAction        = "finalizingAction"
+	TurnFinished            = "turnFinished"
 )
